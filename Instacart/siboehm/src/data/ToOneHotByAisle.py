331c7eaa0a8214f622c0cb7d22ee108eb7a32f73
Simon Böhm <simonboehm@mailbox.org>
2017-07-06 14:22:04 +0200

    Add script to reduce individual products onto their aisle and verify

--- Instacart/siboehm/src/data/ToOneHotByAisle.py
+++ Instacart/siboehm/src/data/ToOneHotByAisle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 from loadData import DataLoader
 from sklearn import preprocessing
 
@@ -17,35 +18,67 @@ order_prod_t = data["order_products__train"]
 # Containing name, aisle and department of each product
 product_info = data["products"]
 
-# Map specific product_id to aisle_id of this product
-order_prod_p_aisle = order_prod_p["product_id"].map(
-    lambda x: product_info[product_info["product_id"] == x].iloc[0][2])
 
-# Replace to product_id with the aisle of this product
-order_prod_p = order_prod_p.drop("product_id", axis=1)
-order_prod_p = order_prod_p.join(order_prod_p_aisle)
-order_prod_p.rename(columns={'product_id': 'aisle_id'}, inplace=True)
+# Warning, function is not side effect free
+def reduceToAisle(df):
+    # Map product to the aisle the product is located in
+    df_aisle = df["product_id"].map(
+        lambda x: product_info[product_info["product_id"] == x].iloc[0][2])
 
-# Remove some columns that are not needed for now
-order_prod_p.drop(["reordered", "add_to_cart_order"], axis=1, inplace=True)
+    # Replace the product id with the aisle of this product
+    df = df.drop("product_id", axis=1)
+    df = df.join(df_aisle)
+    df.rename(columns={'product_id': 'aisle_id'}, inplace=True)
 
-# Make a one hot encoding of the aisles
-binarizer = preprocessing.LabelBinarizer()
-binarized = binarizer.fit_transform(order_prod_p["aisle_id"])
-bin_df = pd.DataFrame(binarized)
-bin_df.columns = binarizer.classes_
+    # Remove some columns not needed for now
+    df.drop(["reordered", "add_to_cart_order"], axis=1, inplace=True)
 
-order_prod_p.drop("aisle_id", axis=1, inplace=True)
-order_prod_p = order_prod_p.join(bin_df)
-order_prod_p = order_prod_p.groupby("order_id").sum()
+    # Make a one hot encoding of the aisles
+    binarizer = preprocessing.LabelBinarizer()
+    binarized = binarizer.fit_transform(df["aisle_id"])
+    bin_df = pd.DataFrame(binarized)
+    classes_binarized = binarizer.classes_
 
-master_p = pd.merge(order_prod_p, orders, right_on="order_id", left_index=True)
-master_p.drop(
-    [
-        "order_id", "order_dow", "days_since_prior_order", "order_hour_of_day",
-        "order_number"
-    ],
-    axis=1,
-    inplace=True)
-master_p_user = master_p.groupby("user_id").sum()
-master_p_user.to_csv("ReduceToAisle.csv")
+    # Fix overflow of classes :/
+    for i in range(len(classes_binarized)):
+        if classes_binarized[i] < 0:
+            classes_binarized[i] = classes_binarized[i] + 128 + 128
+
+    bin_df.columns = classes_binarized
+
+    df.drop("aisle_id", axis=1, inplace=True)
+    df = df.join(bin_df)
+    df = df.groupby("order_id").sum()
+
+    master = pd.merge(df, orders, right_on="order_id", left_index=True)
+    master.drop(
+        [
+            "order_id", "order_dow", "days_since_prior_order",
+            "order_hour_of_day", "order_number"
+        ],
+        axis=1,
+        inplace=True)
+    master_user = master.groupby("user_id").sum()
+    return master_user
+
+
+# Transforming 1.000.000 individual products order takes about 10 minutes on my system
+
+print("Transforming prior...")
+start_time = datetime.datetime.now()
+order_prod_p = reduceToAisle(order_prod_p)
+duration = datetime.datetime.now() - start_time
+print("Transformation finished after {} hours, {} minutes, {} seconds".format(
+    duration.seconds // 3600, duration.seconds // 60 % 60, duration.seconds %
+    60))
+order_prod_p.to_csv("../../data/processed/ReducedToAislePrior.csv")
+
+print("Transforming train...")
+start_time = datetime.datetime.now()
+order_prod_t = reduceToAisle(order_prod_t)
+duration = datetime.datetime.now() - start_time
+print("Transformation finished after {} hours, {} minutes, {} seconds".format(
+    duration.seconds // 3600, duration.seconds // 60 % 60, duration.seconds %
+    60))
+
+order_prod_p.to_csv("../../data/processed/ReducedToAisleTrain.csv")
